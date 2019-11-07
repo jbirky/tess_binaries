b9d303ef55ccc96ad3b432a780582fba533ad3a0
jbirky <jbirky@uw.edu>
2019-11-07 12:20:48 -0800

    reorganize

--- code/test.py
+++ code/test.py
@@ -16,16 +16,34 @@ rc('figure', facecolor='w')
 rc('xtick', labelsize=20)
 rc('ytick', labelsize=20)
 
-from astropy import units as u
-from astropy.io import fits, ascii
-from astropy.table import Table, vstack
-from astropy.timeseries import TimeSeries, LombScargle, BoxLeastSquares, aggregate_downsample
+from tess_binaries import *
 
-tess_dir = os.environ['TESS_DATA']
-lc_dir = tess_dir + '/light_curves'
-sb_dir = tess_dir + '/simbad'
-ps_dir = tess_dir + '/combined_sector_power_spectra'
+def loadSampleFromHDF5(fname)
+    ff = h5py.File(fname, mode="r")
+
+    df = {}
+    for key in list(ff):
+        try:
+            df[key] = ff[key].value
+        except:
+            df[key] = np.array(ff[key].value, dtype='str')
+
+    ff.close()
+    sample = pd.DataFrame(data=df)
+    
+    return sample
+
+
+def loadPickle(ID):
+    ls_period, bls_period = [], []
+
+    infile = open(f'{tb.ps_dir}/{ID}_ps.pkl','rb')
+    ps_dict = pickle.load(infile)
+    infile.close()
+    
+    return ps_dict['ls_best_period']
 
 
 if __name__ == "__main__":
-	print('a')
+	sample = loadSampleFromHDF5(f'{tb.cat_dir}/asassn_tess_inspected.hdf5')
+    
