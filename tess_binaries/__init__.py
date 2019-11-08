f92c43cbc6170eade656a97ffa66d77d1c7a4fc3
jbirky <jbirky@ucsd.edu>
2019-11-07 17:40:30 -0800

    create directories for results

--- tess_binaries/__init__.py
+++ tess_binaries/__init__.py
@@ -5,7 +5,14 @@ from .dtw import *
 
 import os
 
-data_dir = os.environ['TESS_DATA']
-cat_dir  = '../catalogs'
-ps_dir   = '../results/combined_sector_power_spectra'
-plot_dir = '../results/plots'
+tb_path  = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))		# tess_binaries code directory
+data_dir = os.environ['TESS_DATA']											# raw tess fits data files
+cat_dir  = tb_path + '/catalogs'											# catalog files (csv)
+lc_dir   = tb_path + '/results/light_curves'								# combined sector light curves
+ps_dir   = tb_path + '/results/power_spectra'								# combined sector power spectra
+plot_dir = tb_path + '/results/plots'
+
+
+for directory in [cat_dir, lc_dir, ps_dir, plot_dir]:		# create directories to read/store results
+	if os.path.exists(directory) == False:
+		os.system(f'mkdir {directory}')
