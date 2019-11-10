13e475c90a0014d9921c6e05cce0dc4176f6f89a
jbirky <jbirky@uw.edu>
2019-11-10 14:19:25 -0800

    plot fn

--- tess_binaries/core.py
+++ tess_binaries/core.py
@@ -1,3 +1,15 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+from matplotlib import rc
+plt.style.use('classic')
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+rc('text', usetex=True)
+rc('figure', facecolor='w')
+rc('xtick', labelsize=20)
+rc('ytick', labelsize=20)
+
 import tess_binaries as tb
 
 __all__ = ['LightCurve', 'PowerSpectrum', 'Sample']
@@ -10,6 +22,7 @@ class LightCurve():
 	def __init__(self, *args, **kwargs):
 
 		self.tic_id = kwargs.get('tic_id')			# TESS input catalog ID
+        self.type   = kwargs.get('type', None)
 
 		if ('time' in kwargs) and ('flux' in kwargs):
 			self.time 		= kwargs.get('time')		
@@ -21,6 +34,13 @@ class LightCurve():
 			self.time 		= lc[0]					# time in MJD
 			self.flux 		= lc[1]					# PDCSAP flux
 			self.flux_err 	= lc[2]					# PDCSAP flux error
+            
+    def powerSpectrum(**kwargs):
+        
+        method  = kwargs.get('method', 'ls')
+        self.ps = tb.loadPowerSpectrum(self.tic_id)
+        
+        return self.ps
 
 	def phaseFold(period):
 
@@ -32,6 +52,26 @@ class LightCurve():
 		fold_flux = None
 
 		return fold_flux
+    
+    def plot(**kwargs):
+    
+        plt.figure(figsize=[16,8])
+        plt.ticklabel_format(useOffset=False)
+        if self.type != None:
+            plt.plot(self.time, self.flux/np.nanmedian(self.flux), \
+                     color='k', linewidth=.5, label=f'{label}'.replace('_', ' '))
+        else:
+            plt.plot(self.time, self.flux/np.nanmedian(self.flux), color='k', linewidth=.5)
+        plt.ylabel('PDCSAP Flux', fontsize=18)
+        plt.xlabel('Julian Date', fontsize=18)
+        plt.xlim(min(self.time), max(self.time))
+        plt.legend(loc='upper right', frameon=False, fontsize=16)
+        plt.minorticks_on()
+
+        if 'save_dir' in kwargs:
+            save_dir = kwargs.get('save_dir')
+            plt.savefig(f'{save_dir}/TIC_{self.tic_id}.png')
+        plt.show() 
 
 
 class PowerSpectrum():
